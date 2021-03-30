86b27113f72fbdc056af9a8a7cf20d2ef20ab3f5
Phillip McDonald <phillipmcdonaldcode@gmail.com>
2021-03-30 12:10:39 -0400

    Added an array of quotes for the random quote generator
    I added 6 objects to an array, containing the properties of the quotes that will be used by the random

--- js/script.js
+++ js/script.js
@@ -11,12 +11,47 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes =[
 
+  {
+    quote: "You miss 100% of the shots you dont take.",
+    source: "Wayne Gretzkye",
+    year: 1983
+  },
+  {
+    quote: "Be yourself; everyone else is taken.",
+    source:"Oscar Wilde"
+  },
+  {
+    quote: "Just keep swimming.",
+    source: "Dorey",
+    citation: "Finding nemo",
+    year: 2003
+  },
+  {
+    quote: "Why so serious?",
+    source: "The Joker",
+    citation: "The Dark Knight",
+    yeat: 2008
+  },
+  {
+    quote: "May the force be with you.",
+    source: "Obi Wan Kanobi",
+    citation: "Star Wars",
+    year: 1977
+  },
+  {
+    quote: "You only loose what you cling to",
+    source: "Buddah",
+  }
+]
+
+console.log(quotes)
 
 /***
  * `getRandomQuote` function
 ***/
-
+function getRandomQuote(){};
 
 
 /***
